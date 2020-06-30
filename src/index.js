7c9ad3d1f694b64a3095d06b7877d59110b84d51
Павел Рузанкин <48125800+pdelson@users.noreply.github.com>
2020-06-30 13:34:18 +0300

    layout

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import "./scss/index.scss"
+
+const iconMenu = document.querySelector(".icon-menu")
+const menu = document.querySelector(".menu")
+
+function toggle(){
+    const classes = [...iconMenu.classList]
+    if(classes.includes("open")) {
+        iconMenu.classList.remove("open")
+        menu.classList.remove("menu_show")
+    }else{
+        iconMenu.classList.add("open")
+        menu.classList.add("menu_show")
+    }
+}
+
+iconMenu.addEventListener("click",toggle)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
